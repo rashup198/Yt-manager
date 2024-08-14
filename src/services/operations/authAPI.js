d93d68744fb803b9df16455ec2af17659a19ebaf
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2024-08-15 03:15:34 +0530

    updated the logout, getPasswordResetToken,resetPassword

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -110,3 +110,62 @@ export function login(email,password,navigate){
         toast.dismiss(toastId)
     }
 }
+
+export function logout(navigate) {
+    return (dispatch) => {
+      dispatch(setToken(null))
+      dispatch(setUser(null))
+      localStorage.removeItem("token")
+      localStorage.removeItem("user")
+      toast.success("Logged Out")
+      navigate("/")
+    }
+  }
+
+export function getPasswordResetToken(email, setEmailSent){
+    return async(dispatch)=>{
+        dispatch(setLoading(true));
+
+        try {
+            const response=  await apiConnector("POST", RESETPASSTOKEN_API,{
+                email,
+            })
+            console.log("Reset password token response ........", response);
+
+            if(!response.data.success){
+                throw new Error(response.data.message)
+            }
+            toast.success("Reset Email Sent")
+            setEmailSent(true)
+            
+        } catch (error) {
+            console.log("RESET PASSWORD TOKEN Error", error);
+            toast.error("Failed to send email for resetting password");
+        }
+        dispatch(setLoading(false))
+    }
+}
+
+export function resetPassword(password,confirmPassword,token){
+    return async(dispatch)=>{
+        dispatch(setLoading(true));
+
+        try {
+            const response = await apiConnector("POST", RESETPASSWORD_API,{
+                password,
+                confirmPassword,
+                token
+            })
+            console.log("RESET password resposen ........", response);
+            if(!response.data.success){
+                throw new Error(response.data.message)
+            }
+            toast.success("Password has been reset successfully")
+            
+        } catch (error) {
+            console.log("Reset password token error", error);
+            toast.error("Unable to reset the password")
+        }
+        dispatch(setLoading(false));
+    }
+}
